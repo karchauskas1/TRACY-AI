88780694815472dca806ddcfc6bbd89c0571c6a9
Danila Karchauskas <prod@kardd.ru>
2026-01-13 01:25:52 +0300

    fix: добавлен 'use client' в todo-lists/[id]/page.tsx для исправления ошибки деплоя

--- web-app/app/todo-lists/[id]/page.tsx
+++ web-app/app/todo-lists/[id]/page.tsx
@@ -1,11 +1,7 @@
+"use client"
+
 import { useParams } from "next/navigation"
 import { TodoListDetailClient } from "./TodoListDetailClient"
-
-// Для static export Next.js требуется generateStaticParams
-export async function generateStaticParams() {
-  // Возвращаем пустой массив, так как списки создаются динамически
-  return []
-}
 
 export default function TodoListDetailPage() {
   const params = useParams()
