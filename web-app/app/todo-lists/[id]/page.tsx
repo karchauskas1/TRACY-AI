1ee865912721a1c7a932c2bd5469b1ffe36c14cc
Danila Karchauskas <prod@kardd.ru>
2026-01-13 01:26:53 +0300

    fix: разделен server и client компоненты для todo-lists/[id] с generateStaticParams

--- web-app/app/todo-lists/[id]/page.tsx
+++ web-app/app/todo-lists/[id]/page.tsx
@@ -1,15 +1,11 @@
-"use client"
+import { TodoListWrapper } from "./TodoListWrapper"
 
-import { useParams } from "next/navigation"
-import { TodoListDetailClient } from "./TodoListDetailClient"
+// Для static export Next.js требуется generateStaticParams
+export async function generateStaticParams() {
+  // Возвращаем пустой массив, так как списки создаются динамически
+  return []
+}
 
 export default function TodoListDetailPage() {
-  const params = useParams()
-  const listId = params?.id ? parseInt(params.id as string) : null
-  
-  if (!listId) {
-    return <div>Invalid list ID</div>
-  }
-  
-  return <TodoListDetailClient listId={listId} />
+  return <TodoListWrapper />
 }
